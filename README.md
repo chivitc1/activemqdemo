834598c846edb6a46612bd7269886e3511edcfdf
chivitc1 <chivitc1@gmail.com>
2018-10-01 17:13:13 +0700

    Pure java activemq demo: setup project base

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # activemqdemo
+## Pure Java Activemq demo
+- Setup project base
+
+- Run demo:
+
+cd purejava/
+
+./gradlew clean build
+
+./gradlew runApp
---
+++ purejava/src/main/java/com.example.activemqdemo/ActivemqDemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.activemqdemo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ActivemqDemoApplication {
+	public static void main(String[] args) {
+		log.info("ActivemqDemoApplication started");
+	}
+}
